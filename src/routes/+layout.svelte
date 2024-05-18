84eecec0ddda3996e80dfdb8fbc85a40c4fb831a
SSJMUNDEN2 <132748138+SSJMUNDEN2@users.noreply.github.com>
2024-05-18 14:48:18 +0200

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,30 +2,48 @@
   import "./styles.css";
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">KI-Anwendung Demo</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">KI-Anwendung Demo</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/appartment">Appartment Regression</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+      <div class="position-sticky">
+        <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/appartment">Appartment Regression</a>
+            <a class="nav-link text-light" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/appartment">Appartment Regression</a>
           </li>
         </ul>
       </div>
-    </div>
-  </nav>
-
-<div class="container mt-3">
-  <slot />
+    </nav>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="container mt-3">
+        <slot />
+      </div>
+    </main>
+  </div>
 </div>
