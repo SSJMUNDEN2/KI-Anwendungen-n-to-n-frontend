50b32de69f979c4ef03ef9187c3f428179fe6efa
SSJMUNDEN2 <132748138+SSJMUNDEN2@users.noreply.github.com>
2024-05-21 14:21:02 +0200

    Update +page.svelte

--- src/routes/appartment/+page.svelte
+++ src/routes/appartment/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { PUBLIC_BASE_URL } from '$env/static/public';
- 	import axios from "axios";
-
+	import axios from "axios";
+ 
 	let bfs_number_names = [
 		{id: 261, name: `Zürich` },
 		{id: 62, name: `Kloten` },
@@ -108,12 +108,12 @@
 		{id: 51, name: `Bachenbülach` },
 		{id: 295, name: `Horgen` }
 	];
-
+ 
 	let rooms;
 	let area;
 	let price = '...'
 	let selected;
-function handleSubmit() {
+	function handleSubmit() {
     let url =
         PUBLIC_BASE_URL +
         "/api/predict/?bfs_number=" +
@@ -129,73 +129,32 @@ function handleSubmit() {
         console.error('Error fetching data:', error);
     });
 }
+ 
 </script>
-
-<style>
-	.container {
-		background-color: #f7f7f7;
-		padding: 20px;
-		border-radius: 10px;
-		max-width: 800px;
-		margin: 20px auto;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	}
-	h1 {
-		color: #334;
-		font-size: 24px;
-		margin-bottom: 20px;
-	}
-	.form-control, .form-select, .btn {
-		border-radius: 5px;
-	}
-	.form-control {
-		border: 1px solid #ccc;
-		padding: 10px;
-	}
-	.form-select {
-		border: 1px solid #ccc;
-		padding: 10px;
-	}
-	.btn-primary {
-		background-color: #04ff00;
-		border: none;
-		color: #fff;
-		padding: 10px 20px;
-		margin-right: 10px;
-		cursor: pointer;
-	}
-	.btn-secondary {
-		background-color: #6c757d;
-		border: none;
-		color: #fff;
-		padding: 10px 20px;
-		cursor: pointer;
-	}
-</style>
-
 <div class="container text-center">
-	<h1>Are you paying too much? Our model predicts that you should be paying {price}</h1>
-
+ 
+	<h1>Are you paying to much? Our model predicts that you should be paying {price}</h1>
+ 
 	<div class="row justify-content-md-center">
-	  <div class="col col-lg-2">
-		<input type="number" class="form-control" placeholder="Area" aria-label="area" bind:value={area}>
-	  </div>
-	  <div class="col col-lg-2">
-		<input type="number" class="form-control" placeholder="Rooms" aria-label="rooms" bind:value={rooms}>
-	  </div>
-	  <div class="col col-lg-2">
-		<select class="form-select" bind:value={selected}>
-			<option value="">City</option>
-			{#each bfs_number_names as bfs}
-				<option value={bfs}>
-					{bfs.name}
-				</option>
-			{/each}
-		</select>
-	  </div>
-	  <div class="col-md-auto">
-		<button type="button" class="btn btn-primary" on:click={handleSubmit}>Price Estimation</button>
-		<button type="button" class="btn btn-secondary" on:click={handleReset}>Reset</button>
-	  </div>
-	</div>
+<div class="col col-lg-2">
+<input type="number" class="form-control" placeholder="area" aria-label="area" bind:value={area}>
 </div>
+<div class="col col-lg-2">
+<input type="number" class="form-control" placeholder="rooms" aria-label="rooms" bind:value={rooms}>
+</div>
+<div class="col col-lg-2">
+ 
+		<select class="form-select" bind:value={selected}>
+			{#each bfs_number_names as bfs}
+<option value={bfs}>
+					{bfs.name}
+</option>
+			{/each}
+</select>
+</div>
+<div class="col-md-auto">
+<button type="button" class="btn btn-primary" on:click={handleSubmit}>price estimation</button>
+</div>
+</div>
+</div>
+hat Kontextmenü
